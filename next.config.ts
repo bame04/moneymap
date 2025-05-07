bb38ae5e61faa953444b5e54c4572e504a825605
Bame <bamemonageng04@gmail.com>
2025-05-08 01:00:17 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
-    config.resolve.alias.canvas = false
-    config.resolve.alias.encoding = false
-    
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+
     // Configure PDF.js worker
     config.module.rules.push({
       test: /pdf\.worker\.js$/,
       type: 'asset/resource',
       generator: {
-        filename: 'static/worker/[hash][ext][query]'
-      }
-    })
+        filename: 'static/worker/[hash][ext][query]',
+      },
+    });
 
-    return config
-  }
+    return config;
+  },
 };
 
 export default nextConfig;
+
+
+
